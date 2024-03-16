033997d49657a57472222917df85c448442575ef
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-03-16 22:21:54 +0800

    :sparkles: use cppumock for birthday test

--- main/main.cpp
+++ main/main.cpp
@@ -2,8 +2,8 @@
 #include "Birthday.h"
 
 int main() {
-//    Birthday birthday;
-//    std::cout << "is birthday: " << birthday.IsBirthday() << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+    Birthday birthday;
+    std::cout << "is birthday: " << birthday.IsBirthday() << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
--- test/BirthdayTest.cc
+++ test/BirthdayTest.cc
@@ -1,24 +1,44 @@
 #include "CppUTest/TestHarness.h"
 #include "Birthday.h"
+#include "CppUTestExt/MockSupport.h"
 
 TEST_GROUP(Birthday) {
+    void setup() {
+    }
+
+    void teardown() {
+        mock().checkExpectations();
+        mock().clear();
+    }
 };
 
 extern "C" {
 #include "Today.h"
 }
 
-std::tm* date;
+//std::tm* date;
+//
+//std::tm* GetToday() {
+//    return date;
+//}
+//
+//void SetToday(int month, int day) {
+//    std::time_t currentTime = std::time(NULL);
+//    date = std::localtime(&currentTime);
+//    date->tm_mon = month - 1;
+//    date->tm_mday = day;
+//}
 
 std::tm* GetToday() {
-    return date;
+    return (std::tm*)mock().actualCall("GetToday").returnValue().getObjectPointer();
 }
 
 void SetToday(int month, int day) {
     std::time_t currentTime = std::time(NULL);
-    date = std::localtime(&currentTime);
+    std::tm* date = std::localtime(&currentTime);
     date->tm_mon = month - 1;
     date->tm_mday = day;
+    mock().expectOneCall("GetToday").andReturnValue(date);
 }
 
 TEST(Birthday, IsBirthday) {
